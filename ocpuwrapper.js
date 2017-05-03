f2a9c63d71c33e517c11e9073f17a6c841486b49
Arun Iyer <ariy@microsoft.com>
2017-05-03 09:37:24 +0530

    Updating the js

--- ocpuwrapper.js
+++ ocpuwrapper.js
@@ -1,18 +1,16 @@
 ﻿TwoSampleTesting = {
-    TTest: function(x, y) {
+    TTest: function(x, y, out) {
         ocpu.seturl("//public.opencpu.org/ocpu/library/stats/R")
         var result = "";
         var req = ocpu.call("t.test", { x: x, y: y }, function (session) {
             session.getConsole(function (outtxt) {
                 console.log('result from R: ' + outtxt);
-                result = outtxt;
+                out.result = outtxt;
             });
         });
 
         req.fail(function () {
             console.log("OCPU request for t-test failed!");
         });
-
-        return result;
     }
 }
